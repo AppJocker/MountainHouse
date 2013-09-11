595f38dbd9a03794843f53cc61e152678076ef2e
Viisin <runedcross@gmail.com>
2013-09-12 00:35:58 +0800

    Modifiy Login Page
    Login Java Activity , XML
    AndroidManifest.xml
    strings.xml
    styles.xml
    Add hetrayal at house on the hill.jpg

--- src/com/app/mountainhouse/LoginActivity.java
+++ src/com/app/mountainhouse/LoginActivity.java
@@ -1,28 +1,33 @@
-package com.app.mountainhouse;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
+package com.app.mountainhouse ;
 
-public class LoginActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+import android.os.Bundle      ;
+import android.app.Activity   ;
+import android.view.View      ;
+import android.widget.Button  ;
+import android.content.Intent ;
+
+
+public class LoginActivity extends Activity
+{
+	@Override protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState ) ;
+		setContentView( R.layout.activity_login ) ;
+
+		Button button = (Button) findViewById( R.id.buttonFacebookLogin ) ;
 		
-		Button button = (Button)findViewById(R.id.button_login); 
-		button.setOnClickListener(new Button.OnClickListener(){
-		@Override
-		public void onClick(View arg0) {
-			Intent intent = new Intent();
-			intent.setClass(LoginActivity.this, LobbyActivity.class);
-			startActivity(intent); 
-			LoginActivity.this.finish(); 	
-		}
-		});
-	}
+		button.setOnClickListener( new Button.OnClickListener()
+		{
+			@Override public void onClick( View v )
+			{
+				Intent intent = new Intent() ;
+				intent.setClass( LoginActivity.this , LobbyActivity.class ) ;
+				startActivity( intent ) ;
+				//LoginActivity.this.finish() ;
+			}
+		}) ;
 
+	}
 }
