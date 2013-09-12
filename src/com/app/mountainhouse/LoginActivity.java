a263c5d3242adc1a27388f90a3fe789a15e12040
wayne <wayne@iii.org.tw>
2013-09-12 11:58:31 +0800

    Connect to database class
    新增了 ConnectDatabase Class
    Import jedis.jar lib
    
    createconnect() 建立連線
    
    login(type, ID, Name) type=0 為FB , 其他自訂  ID跟NAME都是從FB得到的
    login會先檢查是否資料庫已有, 沒有的話 就註冊新資料, 有得話就回傳已註冊
    
    setplayername(type, ID, PlayerName) 設定玩家名字
    getplayername(type, ID) 得到玩家名字
    
    LoginActivity.java 則是新增了使用此類別的範例方法

--- src/com/app/mountainhouse/LoginActivity.java
+++ src/com/app/mountainhouse/LoginActivity.java
@@ -4,6 +4,7 @@ package com.app.mountainhouse ;
 
 import android.os.Bundle      ;
 import android.app.Activity   ;
+import android.util.Log;
 import android.view.View      ;
 import android.widget.Button  ;
 import android.content.Intent ;
@@ -11,6 +12,7 @@ import android.content.Intent ;
 
 public class LoginActivity extends Activity
 {
+	ConnectDatabase cd;
 	@Override protected void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState ) ;
@@ -22,12 +24,40 @@ public class LoginActivity extends Activity
 		{
 			@Override public void onClick( View v )
 			{
+				/*example database connect*/ // To small tiger FB login use class example	
+				/*cd = new ConnectDatabase();
+				if(cd.createconnect())
+				{
+					int type = 0; //FB = 0, G+ = 1;
+					String ID = "80"; // get ID from FB
+					String Name = "fu"; // get Name from FB
+					String PlayerName = "";
+					
+					int logintype = cd.login(type, ID, Name);
+					//0 = fail
+					//1 = register success, and set player name
+					//2 = already register, and you can get player name
+					
+					if(logintype == 1)
+					{
+						PlayerName = "ABC";
+						cd.setplayername(type, ID, PlayerName);
+					}else if(logintype == 2)
+					{
+						PlayerName = cd.getplayername(type, ID);
+						if(PlayerName == null)
+						{
+							PlayerName = "ABC";
+							cd.setplayername(type, ID, PlayerName);
+						}
+					}	
+				}*/
+				
 				Intent intent = new Intent() ;
 				intent.setClass( LoginActivity.this , LobbyActivity.class ) ;
 				startActivity( intent ) ;
-				//LoginActivity.this.finish() ;
+				//LoginActivity.this.finish() ;	
 			}
-		}) ;
-
+		}) ;		
 	}
 }
