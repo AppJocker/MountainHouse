2c3bfc8cd198f733c1fd5c4e3ab13f5dc7ab15d5
GeneChu <gene4216@gmail.com>
2013-09-18 13:39:33 +0800

    lobby_ver1

---
+++ src/com/app/mountainhouse/MyProgress.java
@@ -0,0 +1,87 @@
+package com.app.mountainhouse;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+// === define progress bar =======
+//================================
+
+public class MyProgress extends ProgressBar{
+    String text;
+    Paint mPaint;
+     
+    public MyProgress(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+        System.out.println("1");
+        initText(); 
+    }
+     
+    public MyProgress(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+        System.out.println("2");
+        initText();
+    }
+ 
+ 
+    public MyProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+        System.out.println("3");
+        initText();
+    }
+     
+    @Override
+    public synchronized void setProgress(int progress) {
+        // TODO Auto-generated method stub
+        setText(progress);
+        super.setProgress(progress);
+         
+    }
+ 
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+        //this.setText();
+        Rect rect = new Rect();
+        this.mPaint.getTextBounds(this.text, 0, this.text.length(), rect);
+        int x = (getWidth() / 2) - rect.centerX();  
+        int y = (getHeight() / 2) - rect.centerY();  
+        canvas.drawText(this.text, x, y, this.mPaint);  
+    }
+     
+    //初始化，画笔
+    private void initText(){
+        this.mPaint = new Paint();
+        this.mPaint.setColor(Color.WHITE);
+        this.mPaint.setTextSize(18);
+        //去掉锯齿
+        this.mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+         
+    }
+     
+    private void setText(){
+        setText(this.getProgress());
+    }
+     
+    //设置文字内容
+    private void setText(int progress){
+        int i = (progress * 100)/this.getMax();
+        this.text = String.valueOf(i) + "%";
+    }
+     
+     
+}	
