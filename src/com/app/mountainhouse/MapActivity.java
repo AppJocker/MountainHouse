5372a01fdd00307f398e1c608c038f43d8c5641a
Wayne <waynelin@iii.org.tw>
2013-09-24 10:48:07 +0800

    merge_gene_develop

---
+++ src/com/app/mountainhouse/MapActivity.java
@@ -0,0 +1,5 @@
+package com.app.mountainhouse;
+
+public class MapActivity {
+
+}
---
+++ src/com/app/mountainhouse/TaskActivity.java
@@ -0,0 +1,15 @@
+package com.app.mountainhouse;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TaskActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_task);
+		
+	}
+
+}
